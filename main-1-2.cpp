86b2df7911d8d8a1e0dd3025d1c1e20633d60476
Dai Luong <luong@DESKTOP-INLVFFC.localdomain>
2023-03-30 13:25:35 +1030

    Serious test

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -8,43 +8,43 @@ int main() {
     // Bike_yard();
     // cout << "Default capacity: " << Bike_yard.capacity << endl;
 
-    Bike_yard(3);
+    Bike_yard bike_yard(3);
 
     Bike b1("A", 3382);
     Bike b2("B", 5321);
     Bike b3("C", 3382);
     Bike b4("C", 3482);
 
-    if (Bike_yard::add_stock(b1)) {
+    if (bike_yard.add_stock(b1)) {
         cout << "Successfully add bike 1" << endl;
     } else {
         cout << "Unsuccessfully add bike 1" << endl;
     }
 
-    if (Bike_yard::add_stock(b2)) {
+    if (bike_yard.add_stock(b2)) {
         cout << "Successfully add bike 2" << endl;
     } else {
         cout << "Unsuccessfully add bike 2" << endl;
     }
 
-    if (Bike_yard::add_stock(Bike.b3)) {
+    if (bike_yard.add_stock(b3)) {
         cout << "Successfully add bike 3" << endl;
     } else {
         cout << "Unsuccessfully add bike 3" << endl;
     }
 
-    if (Bike_yard::add_stock(Bike.b4)) {
+    if (bike_yard.add_stock(b4)) {
         cout << "Successfully add bike 4" << endl;
     } else {
         cout << "Unsuccessfully add bike 4" << endl;
     }
 
-    cout << "The current quantity is: " << Bike_yard::get_total_stock_count() << endl;
+    cout << "The current quantity is: " << bike_yard.get_total_stock_count() << endl;
 
-    cout << "The number of bikes with code 3382 is: " << Bike_yard::get_stock_quantity(3382) << endl;
+    cout << "The number of bikes with code 3382 is: " << bike_yard.get_stock_quantity(3382) << endl;
 
     Bike *b;
-    b = Bike_yard::get_current_stock_list();
+    b = bike_yard.get_current_stock_list();
     
     // for (int i = 0; i < get_total_stock_count(); i++) {
     //     b[i] = 
